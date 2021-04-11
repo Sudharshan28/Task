6a06d8eadc6c6ef9237b07484e9d658d4ade3c85
Sudharshan28 <82323977+Sudharshan28@users.noreply.github.com>
2021-04-11 21:56:46 +0530

    Add files via upload

---
+++ Movie Finder/js/suggestion.js
@@ -0,0 +1,18 @@
+var myobj;
+
+myobj = {
+	"suggestions": [ "Iron Man",
+    "Avengers",
+    "Captain Marvel",
+    "Captain america",
+    "Thor",
+    "Iron Man Part-2",
+    "Iron Man Part-3",
+    "Ant Man",
+    "Age of Ultrons",
+    "End Game",
+    "Infinity War",
+    "Captain America Part-3",
+    "Captain America Part-2",
+    "Black Panther" ]
+	}
